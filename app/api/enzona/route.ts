52dd26dc259e07278afb143d61511343bb210459
racielfg01 <39809379+racielfg01@users.noreply.github.com>
2024-02-08 13:09:10 -0500

    Update route.ts

--- app/api/enzona/route.ts
+++ app/api/enzona/route.ts
@@ -8,36 +8,25 @@ interface IParams {
   listingId?: string;
 }
 
-export async function POST(request: Request, { params }: { params: IParams }) {
-  const currentUser = await getCurrentUser();
+export async function POST(request: Request) {
+   // Import the necessary libraries.
 
-  if (!currentUser) {
-    return NextResponse.error();
+// Create a new file and write data to it.
+fs.writeFile('success.txt', JSON.stringify(request), (err: any) => {
+  if (err) throw err;
+  console.log('The file has been saved!');
+});
+
+// Close the file.
+fs.close(fs.openSync('data.txt', 'w'), (err: any) => {
+  if (err) throw err;
+  console.log('The file has been closed!');
+});
+
+      return NextResponse.json("Escribio");
+
   }
-
-  const { listingId } = params;
-
-  if (!listingId || typeof listingId !== "string") {
-    throw new Error("Invalid Id");
-  }
-
-  let favoritesIds = [...(currentUser.favoriteIds || [])];
-
-  favoritesIds.push(listingId);
-
-  const user = await prisma.user.update({
-    where: {
-      id: currentUser.id,
-    },
-    data: {
-      favoriteIds: favoritesIds,
-    },
-  });
-
-  return NextResponse.json(user);
-}
-
-
+  
 export async function GET(request: Request) {
    // Import the necessary libraries.
 
